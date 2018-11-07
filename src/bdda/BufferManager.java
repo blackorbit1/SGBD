da9514042b72c8c2730b8d000bb2cb57931ac520
blackorbit1 <enzo.dutra@gmail.com>
2018-11-07 14:48:52 +0100

    classe sans code

--- src/bdda/BufferManager.java
+++ src/bdda/BufferManager.java
@@ -1,7 +1,39 @@
 package bdda;
 
+import java.nio.ByteBuffer;
+
 public class BufferManager {
-	Buffer getPage(iPageId) {
-		
+	//Pour avoir une unique instance de BufferManager
+	private static final BufferManager instance = new BufferManager();
+	private BufferManager() {}
+	public static BufferManager getInstance() {
+		return instance;
+	}
+
+
+	// il faut toujours incrémenter le pin_count
+
+	/** Retourne un des buffers qui stockent le contenu d’une page dans une des cases
+	 * @param iPageId l'ID de la page en question
+	 *
+	 * @return un des buffer qui stockent le contenu de la page
+	 */
+	public ByteBuffer getPage(int iPageId) {// vérifier si la page est déjà chargée
+		return();
+	}
+
+	/** Libérer une page
+	 * @param iPageId l'ID de la page en question
+	 * @param iIsDirty TRUE pour changé FALSE pour inchangé
+	 */
+	public void freePage(PageID iPageId, boolean iIsDirty){
+
+	}
+
+	/** Tout enregistrer sur le disque et mettre les dirty en false
+	 *
+	 */
+	public void flushAll(){
+
 	}
 }
---
+++ src/bdda/Constantes.java
@@ -0,0 +1,6 @@
+package bdda;
+
+public class Constantes {
+    public static final int pageSize = 4096;
+    public static final int frameCount = 2;
+}
