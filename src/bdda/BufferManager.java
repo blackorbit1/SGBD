b51adb2e7e3e836915345bab6884bd4d8c97d85e
Emilie <37456623+Eiiko@users.noreply.github.com>
2018-11-08 22:36:57 +0100

    modification du nom de l'instance

--- src/bdda/BufferManager.java
+++ src/bdda/BufferManager.java
@@ -4,47 +4,59 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
 public class BufferManager {
-    private ArrayList<Frame> frames = new ArrayList<>();
+	private ArrayList<Frame> frames = new ArrayList<>();
 
-	//Pour avoir une unique instance de BufferManager
-	private static final BufferManager instance = new BufferManager();
-	private BufferManager() {}
-	public static BufferManager getInstance() {
-		return instance;
+	// Pour avoir une unique instance de BufferManager
+	private static final BufferManager bufferManager = new BufferManager();
+
+	private BufferManager() {
 	}
 
+	public static BufferManager getInstance() {
+		return bufferManager;
+	}
 
 	// il faut toujours incrémenter le pin_count
 
-	/** Retourne un des buffers qui stockent le contenu d’une page dans une des cases
-	 * @param iPageId l'ID de la page en question
+	/**
+	 * Retourne un des buffers qui stockent le contenu d’une page dans une des
+	 * cases
+	 * 
+	 * @param iPageId
+	 *            l'ID de la page en question
 	 *
 	 * @return un des buffer qui stockent le contenu de la page
 	 */
 	public ByteBuffer getPage(PageId iPageId) {// vérifier si la page est déjà chargée
-	    ByteBuffer bf = ByteBuffer.allocateDirect(Constantes.pageSize);
-        String chaine;
-        for(int i = 0; i<frames.size();i++){
-            if(frames.get(i).getPageIdx() == iPageId.getPageIdx()){
-                int dirty = frames.get(i).isDirty()?1:0;
-                chaine += "///frame@pid=" + frames.get(i).getPageIdx() + "@d=" + dirty + "@pc=" + frames.get(i).getPin_count();
-            }
-        }
-		return(bf.put(chaine));
+		ByteBuffer bf = ByteBuffer.allocateDirect(Constantes.pageSize);
+		String chaine;
+		for (int i = 0; i < frames.size(); i++) {
+			if (frames.get(i).getPageIdx() == iPageId.getPageIdx()) {
+				int dirty = frames.get(i).isDirty() ? 1 : 0;
+				chaine += "///frame@pid=" + frames.get(i).getPageIdx() + "@d=" + dirty + "@pc="
+						+ frames.get(i).getPin_count();
+			}
+		}
+		return (bf.put(chaine));
 	}
 
-	/** Libérer une page
-	 * @param iPageId l'ID de la page en question
-	 * @param iIsDirty TRUE pour changé FALSE pour inchangé
+	/**
+	 * Libérer une page
+	 * 
+	 * @param iPageId
+	 *            l'ID de la page en question
+	 * @param iIsDirty
+	 *            TRUE pour changé FALSE pour inchangé
 	 */
-	public void freePage(PageId iPageId, boolean iIsDirty){
+	public void freePage(PageId iPageId, boolean iIsDirty) {
 
 	}
 
-	/** Tout enregistrer sur le disque et mettre les dirty en false
+	/**
+	 * Tout enregistrer sur le disque et mettre les dirty en false
 	 *
 	 */
-	public void flushAll(){
+	public void flushAll() {
 
 	}
 }
