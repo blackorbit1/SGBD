77ce7300421d493ca4a63be87d86ee98e0c507b8
Emilie <37456623+Eiiko@users.noreply.github.com>
2018-11-08 22:01:31 +0100

    Modification de classe DiskManager + ajout constante

--- src/bdda/DiskManager.java
+++ src/bdda/DiskManager.java
@@ -20,39 +20,58 @@ public class DiskManager {
 		return diskManager;
 	}
 
+	/**
+	 * Cette fonction permet de creer un niveau fichier dont le nom est :
+	 * "Data_(identifiantDuFichier).rf"
+	 * 
+	 * @param iFileIdx identifiant du fichier qu'on creer
+	 * @throws IOException
+	 */
 	public void createFile(int iFileIdx) throws IOException {
-		File file = new File("\\DB\\Data_" + iFileIdx + ".rf");
+		File file = new File(Constantes.pathName + iFileIdx + ".rf");
 		file.createNewFile();
 	}
 
+	/**
+	 * Cette fonction ajoute une nouvelle page de taille 4ko
+	 * @param iFileIdx identifiant du fichier
+	 * @param oPageId de type PageId qui comprend le numero de la page et l'identifant du fichier
+	 * @throws IOException
+	 */
 	public void addPage(int iFileIdx, PageId oPageId) throws IOException {
 		// ajouter la page
-		// ByteBuffer
-		RandomAccessFile rf = new RandomAccessFile("\\DB\\Data_" + Integer.toString(oPageId.getPageIdx()) + ".rf", "r");
-		data = ByteBuffer.allocateDirect(Constantes.pageSize);
-		byte[] tab = new byte[Constantes.pageSize];
-		for (Byte b : tab) {
-			b = 0;
+		try (RandomAccessFile rf = new RandomAccessFile(Constantes.pathName + oPageId.getPageIdx() + ".rf", "r")) {
+			//data = ByteBuffer.allocateDirect(Constantes.pageSize);
+			byte[] tab = new byte[Constantes.pageSize];
+			for (Byte b : tab) {
+				rf.writeByte(0);
+			}
 		}
 		// Postionner à la fin de la dernière page
-		rf.seek(pos);
-
-		rf.write(data.wrap(tab).getInt());
-		rf.close();
-
+		// rf.seek(pos);
+		// rf.write(data.wrap(tab).getInt());
 		oPageId.setFileIdx(iFileIdx);
+	}
+
+	/**
+	 * Permet de lire une page (input)
+	 * @param iPageId
+	 * @param oBuffer
+	 * @throws FileNotFoundException
+	 */
+	public void readPage(PageId iPageId, ByteBuffer iBuffer) throws FileNotFoundException {
+		RandomAccessFile readFile = new RandomAccessFile(Constantes.pathName + iPageId.getPageIdx() + ".rf", "r");
 
 	}
 
-	public void readPage(PageId iPageId, ByteBuffer oBuffer) throws FileNotFoundException {
-		RandomAccessFile readFile = new RandomAccessFile("\\DB\\Data_" + Integer.toString(iPageId.getPageIdx()) + ".rf",
-				"r");
-
-	}
-
-	public void writePage(PageId iPageId, ByteBuffer iBuffer) throws FileNotFoundException {
-		RandomAccessFile writeFile = new RandomAccessFile(
-				"\\DB\\Data_" + Integer.toString(iPageId.getPageIdx()) + ".rf", "w");
+	/**
+	 * Permet d'ecrire sur dans une page (output)
+	 * @param iPageId
+	 * @param oBuffer
+	 * @throws FileNotFoundException
+	 */
+	public void writePage(PageId iPageId, ByteBuffer oBuffer) throws FileNotFoundException {
+		RandomAccessFile writeFile = new RandomAccessFile(Constantes.pathName + iPageId.getPageIdx() + ".rf", "w");
 
 	}
 }
