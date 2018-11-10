ab25a7673f92ad024119ec28b859c404411dbf0d
LounisOB <ob.lounis@gmail.com>
2018-11-10 15:28:25 +0100

    readPage + addPage

--- src/bdda/Constantes.java
+++ src/bdda/Constantes.java
@@ -3,5 +3,5 @@ package bdda;
 public class Constantes {
     public static final int pageSize = 4096;
     public static final int frameCount = 2;
-    public static final String pathName = "\\DB\\Data_";
+    public static final String pathName = "\\BD\\";
 }
