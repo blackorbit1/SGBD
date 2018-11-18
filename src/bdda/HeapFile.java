56e59ecd7a460a24b4eb97c49cc76178d0cfd616
LounisOB <ob.lounis@gmail.com>
2018-11-18 13:24:38 +0100

    HeapFile HeapFile fonction getFreePageId (v2)

--- src/bdda/HeapFile.java
+++ src/bdda/HeapFile.java
@@ -23,6 +23,12 @@ public class HeapFile {
 
 	}
 
+	/*
+	 * Cette méthode doit remplir l’argument oPageId avec l’identifiant d’une page
+	 * de données sur laquelle il reste des cases disponibles. Si cela n’est pas le
+	 * cas, la méthode gére le rajout d’une page (libre) et l’actualisation des
+	 * informations de la Header Page.
+	 */
 	public void getFreePageId(PageId oPageId) {
 		// oPageId.setFileIdx(pointeur.fileIdx);
 		try {
@@ -31,27 +37,36 @@ public class HeapFile {
 			HeaderPageInfo headerPageI = new HeaderPageInfo();
 			headerPageI.readFromBuffer(bufferHeaderPage);
 
+			boolean slotDisponible = false;
 			for (DataPage d : headerPageI.getListePages()) {
 				if (d.getFreeSlots() > 0) {
 					oPageId.setPageIdx(d.getPageIdx());
 					BufferManager.getInstance().freePage(headerpage, false);
-				} else {
-					PageId newpid = new PageId();
-					DiskManager.getInstance().addPage(pointeur.fileIdx, newpid);
-					oPageId.setPageIdx(newpid.getPageIdx());
-					headerPageI.getListePages().add(new DataPage(oPageId.getPageIdx(), pointeur.getSlotCount()));
-
-					headerPageI.writeToBuffer(bufferHeaderPage);
-					BufferManager.getInstance().freePage(headerpage, true);
-
-					ByteBuffer nouvellePage = BufferManager.getInstance().getPage(newpid);
-					// écrire une séquence de 0 au début de la page (bytemap)
-					//
-					//
-
-					BufferManager.getInstance().freePage(newpid, true);
-
+					slotDisponible = true;
+					break;
 				}
+			}
+			if (!(slotDisponible)) {
+
+				PageId newpid = new PageId();
+				DiskManager.getInstance().addPage(pointeur.fileIdx, newpid);
+				oPageId.setPageIdx(newpid.getPageIdx());
+				headerPageI.getListePages().add(new DataPage(oPageId.getPageIdx(), pointeur.getSlotCount()));
+
+				headerPageI.writeToBuffer(bufferHeaderPage);
+				BufferManager.getInstance().freePage(headerpage, true);
+
+				ByteBuffer nouvellePage = BufferManager.getInstance().getPage(newpid);
+
+				// écrire une séquence de 0 au début de la page (bytemap) pour signifier que
+				// toutes les cases sont vides
+				nouvellePage.position(0);
+				for (int i = 0; i < pointeur.getSlotCount(); i++) {
+					nouvellePage.putInt(0);
+				}
+
+				BufferManager.getInstance().freePage(newpid, true);
+
 			}
 
 		} catch (SGBDException e) {
