267c04cf10cd814812bc473fcba1d4829098fa25
blackorbit1 <enzo.dutra@gmail.com>
2018-11-19 00:25:03 +0100

    Création de la classe record avec 1 attribut "values" de type ArrayList<String> + getter et setters

---
+++ src/bdda/Record.java
@@ -0,0 +1,15 @@
+package bdda;
+
+import java.util.ArrayList;
+
+public class Record {
+    ArrayList<String> values;
+
+    public void setValues(ArrayList<String> values) {
+        this.values = values;
+    }
+
+    public ArrayList<String> getValues(){
+        return values;
+    }
+}
