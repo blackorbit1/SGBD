0377e9d8b688191860464e71baa004157dd2a3b8
blackorbit1 <enzo.dutra@gmail.com>
2018-11-19 00:14:47 +0100

    Utilisation des getters et setter au lieu d'une variable qui est en fait privée. J'ai aussi viré des accents parce que ça passe pas chez moi et j'ai fait une correction au niveau du traitement des erreurs

--- src/bdda/HeapFile.java
+++ src/bdda/HeapFile.java
@@ -1,5 +1,6 @@
 package bdda;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import exception.SGBDException;
@@ -23,16 +24,16 @@ public class HeapFile {
 
 	}
 
-	/*
-	 * Cette méthode doit remplir l’argument oPageId avec l’identifiant d’une page
-	 * de données sur laquelle il reste des cases disponibles. Si cela n’est pas le
-	 * cas, la méthode gére le rajout d’une page (libre) et l’actualisation des
+	/**
+	 * Cette methode doit remplir l'argument oPageId avec l'identifiant d'une page
+	 * de donnees sur laquelle il reste des cases disponibles. Si cela n'est pas le
+	 * cas, la methode gere le rajout d'une page (libre) et l'actualisation des
 	 * informations de la Header Page.
 	 */
-	public void getFreePageId(PageId oPageId) {
+	public void getFreePageId(PageId oPageId) throws SGBDException {
 		// oPageId.setFileIdx(pointeur.fileIdx);
 		try {
-			PageId headerpage = new PageId(pointeur.fileIdx, 0);
+			PageId headerpage = new PageId(pointeur.getFileIdx(), 0);
 			ByteBuffer bufferHeaderPage = BufferManager.getInstance().getPage(headerpage);
 			HeaderPageInfo headerPageI = new HeaderPageInfo();
 			headerPageI.readFromBuffer(bufferHeaderPage);
@@ -49,7 +50,7 @@ public class HeapFile {
 			if (!(slotDisponible)) {
 
 				PageId newpid = new PageId();
-				DiskManager.getInstance().addPage(pointeur.fileIdx, newpid);
+				DiskManager.getInstance().addPage(pointeur.getFileIdx(), newpid);
 				oPageId.setPageIdx(newpid.getPageIdx());
 				headerPageI.getListePages().add(new DataPage(oPageId.getPageIdx(), pointeur.getSlotCount()));
 
@@ -58,7 +59,7 @@ public class HeapFile {
 
 				ByteBuffer nouvellePage = BufferManager.getInstance().getPage(newpid);
 
-				// écrire une séquence de 0 au début de la page (bytemap) pour signifier que
+				// Ecrire une sequence de 0 au debut de la page (bytemap) pour signifier que
 				// toutes les cases sont vides
 				nouvellePage.position(0);
 				for (int i = 0; i < pointeur.getSlotCount(); i++) {
@@ -68,10 +69,8 @@ public class HeapFile {
 				BufferManager.getInstance().freePage(newpid, true);
 
 			}
-
-		} catch (SGBDException e) {
-
-			e.printStackTrace();
+		} catch (IOException e) {
+			throw new SGBDException("Erreur d'I/O lors de la crÃ©ation d'une page");
 		}
 	}
 
