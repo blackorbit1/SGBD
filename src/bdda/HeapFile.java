6de8aad8416a2364b74574966dd5a963cc630a71
LounisOB <ob.lounis@gmail.com>
2018-11-18 12:11:10 +0100

    HeapFile fonction getFreePageId(PageId oPageId) + classe PageId

--- src/bdda/HeapFile.java
+++ src/bdda/HeapFile.java
@@ -1,29 +1,67 @@
 package bdda;
 
+import java.nio.ByteBuffer;
+
+import exception.SGBDException;
+
 public class HeapFile {
 	private RelDef pointeur;
-
 
 	public RelDef getPointeur() {
 		return pointeur;
 	}
+
 	public void setPointeur(RelDef pointeur) {
 		this.pointeur = pointeur;
 	}
-	
+
 	/**
-	 * Gerer la création du fichier disque correspondant et le rajout d’une HeaderPage « vide » à ce fichier
+	 * Gerer la création du fichier disque correspondant et le rajout d’une
+	 * HeaderPage « vide » à ce fichier
 	 */
-	public void createNewOnDisk () {
-		
+	public void createNewOnDisk() {
+
 	}
-	
+
 	public void getFreePageId(PageId oPageId) {
-		
+		// oPageId.setFileIdx(pointeur.fileIdx);
+		try {
+			PageId headerpage = new PageId(pointeur.fileIdx, 0);
+			ByteBuffer bufferHeaderPage = BufferManager.getInstance().getPage(headerpage);
+			HeaderPageInfo headerPageI = new HeaderPageInfo();
+			headerPageI.readFromBuffer(bufferHeaderPage);
+
+			for (DataPage d : headerPageI.getListePages()) {
+				if (d.getFreeSlots() > 0) {
+					oPageId.setPageIdx(d.getPageIdx());
+					BufferManager.getInstance().freePage(headerpage, false);
+				} else {
+					PageId newpid = new PageId();
+					DiskManager.getInstance().addPage(pointeur.fileIdx, newpid);
+					oPageId.setPageIdx(newpid.getPageIdx());
+					headerPageI.getListePages().add(new DataPage(oPageId.getPageIdx(), pointeur.getSlotCount()));
+
+					headerPageI.writeToBuffer(bufferHeaderPage);
+					BufferManager.getInstance().freePage(headerpage, true);
+
+					ByteBuffer nouvellePage = BufferManager.getInstance().getPage(newpid);
+					// �crire une s�quence de 0 au d�but de la page (bytemap)
+					//
+					//
+
+					BufferManager.getInstance().freePage(newpid, true);
+
+				}
+			}
+
+		} catch (SGBDException e) {
+
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void updateHeaderWithTakenSlot(PageId iPageId) {
-		
+
 	}
 
 }
--- src/bdda/PageId.java
+++ src/bdda/PageId.java
@@ -4,6 +4,16 @@ public class PageId {
 	private int FileIdx;
 	private int PageIdx;
 
+	public PageId(int FileId, int PageId) {
+		this.FileIdx = FileId;
+		this.PageIdx = PageId;
+	}
+
+	public PageId() {
+		this.FileIdx = 0;
+		this.PageIdx = 0;
+	}
+
 	public int getFileIdx() {
 		return FileIdx;
 	}
