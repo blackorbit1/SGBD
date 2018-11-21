7fae994e0da9862eb6776d4b0b823a6374030f06
Emilie <37456623+Eiiko@users.noreply.github.com>
2018-11-21 14:27:08 +0100

    Classe Rid

---
+++ src/bdda/Rid.java
@@ -0,0 +1,24 @@
+package bdda;
+
+public class Rid {
+
+	private PageId pageId;
+	private int slotIdx;
+
+	public PageId getPageId() {
+		return pageId;
+	}
+
+	public void setPageId(PageId pageId) {
+		this.pageId = pageId;
+	}
+
+	public int getSlotIdx() {
+		return slotIdx;
+	}
+
+	public void setSlotIdx(int slotIdx) {
+		this.slotIdx = slotIdx;
+	}
+
+}
